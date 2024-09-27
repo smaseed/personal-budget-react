6606cd8c2c444e88f8a84a017c070b9617259300
Shabbir Khan <shabbirkhan@Shabbirs-MacBook-Air.local>
2024-09-27 10:29:14 -0400

    Added React version of Personal Budget

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  Route,
+  Routes
+} from 'react-router-dom';
+
+
+import Menu from './Menu/Menu';
+import Hero from './Hero/Hero';
+import HomePage from './HomePage/HomePage';
+import Footer from './Footer/Footer';
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/LoginPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu/>
+      <Hero/>
+      <div className='mainContainer'>
+          <Routes>
+            <Route path='/' element={<HomePage/>}></Route>
+            <Route path='/about' element={<AboutPage/>}></Route>
+            <Route path='/login' element={<LoginPage/>}></Route>
+          </Routes>
+      </div>
+      <Footer/>
     </div>
   );
 }
